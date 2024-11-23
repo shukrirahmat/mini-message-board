f672c1f71e5d19975b7c05241be1233f813d7bbc
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2024-11-23 22:45:02 +0800

    basic server setup

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("HELLO WORLD")
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}...`);
+})
