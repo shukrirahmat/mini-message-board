ba901b45d780e53bea3e0980578bf2d3698a6e0f
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2024-11-23 23:21:08 +0800

    set up routes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
+const indexRouter = require("./routes/indexRouter");
+const newMessageRouter = require("./routes/newMessageRouter");
+
+
+
+app.use("/", indexRouter);
+app.use("/new", newMessageRouter)
 
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
-
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}...`);
 })
