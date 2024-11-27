9cd9a321e90e00fa0a5b642adb90a7be96ac0da6
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2024-11-27 12:40:57 +0800

    add navigation links and forms for new msg

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const path = require("node:path");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/", indexRouter);
+app.use("/", indexRouter.router);
 app.use("/new", newMessageRouter)
 
 const PORT = process.env.PORT || 3000;
--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -1,8 +1,17 @@
 const {Router} = require('express');
 const router = Router();
+const messages = require('./indexRouter').messages;
 
 router.get("/", (req, res) => {
-    res.send("YOU ARE IN NEW MESSAGE ROUTE")
+    res.render('form', { title: "Form"});
+})
+
+router.post("/", (req, res) => {
+    const user = req.body.username;
+    const text = req.body.messageText;
+    messages.push({user, text, added: new Date()});
+    res.redirect("/");
+    
 })
 
 module.exports = router;
